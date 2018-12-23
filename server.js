f19f9c5e2e3d2d71d211936ee045aa77f4b548bc
Aqib Javed <aqibjaved50@gmail.com>
2018-12-23 21:40:31 +0500

    users route - part 1

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-
-
-const db= require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
-
 mongoose
-.connect(db)
-.then(()=> console.log('MongoDB Connected'))
-.catch(err => console.log(err));
-
-
-app.get('/', (req,res)=> res.send('Hello eofg'));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // Use Routes
 app.use('/api/users', users);
@@ -28,5 +29,4 @@ app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, ()=> console.log(`Server is running on port ${port}`));
- 
+app.listen(port, () => console.log(`Server running on port ${port}`));
