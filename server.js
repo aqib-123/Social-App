5738b745137eb45523e1446eadb7e76676014611
Aqib Javed <aqibjaved50@gmail.com>
2018-12-25 11:47:26 +0500

    users route - part 3(authorization (with jwt-strategy)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+const passport = require('passport');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -21,6 +21,14 @@ mongoose
   .connect(db)
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
+
+
+  // Passport middleware
+app.use(passport.initialize());
+
+// Passport Config
+require('./config/passport')(passport);
+
 
 // Use Routes
 app.use('/api/users', users);
